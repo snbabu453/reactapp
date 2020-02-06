bf6a7b58a28c85210f55057fc533cc7ce26f2c23
snbabu453 <snbabu453@gmail.com>
2020-02-06 11:17:12 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and updated to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://www.imaginnovate.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Imaginnovate website
         </a>
       </header>
     </div>
